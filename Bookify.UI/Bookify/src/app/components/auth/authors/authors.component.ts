9ff23aa7554beaf681b7444875495ccc32f95d75
OsamaBytes <osama.ahmed@46bytes.com>
2022-07-29 23:20:50 +0500

    Author Table

--- Bookify.UI/Bookify/src/app/components/auth/authors/authors.component.ts
+++ Bookify.UI/Bookify/src/app/components/auth/authors/authors.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Author } from 'src/app/models/Author.model';
+import { AuthorService } from 'src/app/services/Author.Service/author.service';
 
 @Component({
   selector: 'app-authors',
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.css']
 })
-export class AuthorsComponent implements OnInit {
+export class AuthorsComponent implements AfterViewInit {
+  authorTableColumn: string[] = ['name', 'description'];
 
-  constructor() { }
+  dataSource: MatTableDataSource<Author>;
 
-  ngOnInit(): void {
+  authors: Author[] = [];
+
+  constructor(private authorService: AuthorService) { 
+    this.authors = this.authorService.AllAuthors();
+    this.dataSource = new MatTableDataSource<Author>(this.authors);
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator | any;
+  @ViewChild(MatSort) sort: MatSort | any;
+  
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;    
+  }
+
+  applyFilter(event: Event){
+    var filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if(this.dataSource.paginator){
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
---
+++ Bookify.UI/Bookify/src/app/services/Author.Service/author.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Author } from 'src/app/models/Author.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorService {
+
+  constructor() { }
+
+  public AllAuthors(): Author[]{
+    let authors: Author[] = [
+      {
+        id: '00000000-0000-0000-0000-000000000000',
+        name: 'Osama Ahmad',
+        description: 'Author Specialized for writing tech books'
+      },
+      {
+        id: '00000000-0000-0000-0000-000000000000',
+        name: 'Ali Raza',
+        description: 'Author Specialized for writing life physicology books'
+      }
+    ];
+
+    return authors;
+  }
+}
